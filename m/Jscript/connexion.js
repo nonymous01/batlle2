07fe651ca972078b58492701180cfa601b1d8e13
nonymous <youssef.anonymous0@gmail.com>
2023-09-30 12:34:14 +0000

    d

---
+++ m/Jscript/connexion.js
@@ -0,0 +1,43 @@
+let boutton = document.querySelector("#connexion")
+let formul = document.querySelector("form")
+formul.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    let userlist = JSON.parse(localStorage.getItem("tab_LS"))
+    let result ;
+    userlist.forEach((user) => {
+        if (document.querySelector("#nom").value == user.nom && document.querySelector("#mot_de_passe").value ===user.mot_de_passe){
+            result = user
+        }
+    });
+    if (typeof result !=="undefined"){
+        localStorage.setItem("session", JSON.stringify(result))
+        window.location.replace("./../acceuil_com/index.html");
+    }
+
+    else if (document.querySelector("#nom").value ==="Anonymous" && document.querySelector("#mot_de_passe").value ==="Anonymous"){
+        alert("Bienvenus ✞ ANØNYMØÜ$ ✞ ♤JOKER♤ !")
+        admin_Anonyme ={
+            nom:nom,
+            mot_de_passe:mot_de_passe
+        }
+        userlist.push(admin_Anonyme)
+        localStorage.setItem('tab_LS', JSON.stringify(userlist))
+        window.location.replace("../admin/adimn.html")
+    }
+    else{ 
+            alert("nom ou mot de passe incorrect !")
+            window.location.reload()
+    }
+
+    // let nom = document.querySelector("#nom").value
+    // let mot_de_passe = document.querySelector("#mot_de_passe").value
+
+    
+
+    
+    userlist.push(user)
+    localStorage.setItem("tab_LS", JSON.stringify(userlist))
+})
+
+
+
